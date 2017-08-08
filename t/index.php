fa8574f6743e9380401aa736ca994a4eb8728d86
puneetk00 <puneetk00@gmail.com>
2017-08-08 14:27:20 +0530

    Adsense script
    Adsense script

---
+++ t/index.php
@@ -0,0 +1,36 @@
+<?php 
+	if($_GET['r'] != ''){
+		header('Location: http://synapse.asia/marketplace9196/t/?q='. base64_encode($_GET['r']));
+	}
+	
+?>
+<style>
+	.funn { max-width:100%; }
+	.funn img { width:100%; }
+	.funn p {
+    position: relative;
+    background: #000;
+    color: #fff;
+    padding: 10px;
+    width: 100%;
+    margin: 0;
+}
+</style>
+<div class="funn">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-format="fluid"
+     data-ad-layout="in-article"
+     data-ad-client="ca-pub-3010449844662215"
+     data-ad-slot="3522344982"></ins>
+	<script>
+		 (adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+<img src="images.png">
+<p><?php echo base64_decode($_GET['q']); ?></p>
+</div>
+<form action="" method="GET">
+	<input type="text" name="r">
+<input type="submit">
+</form>
